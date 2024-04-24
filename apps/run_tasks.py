bef253028e9b741e601ad9463457449178665870
BruneelRobin <36740717+BruneelRobin@users.noreply.github.com>
2024-04-24 11:01:32 +0200

    Update run_tasks.py

--- apps/run_tasks.py
+++ apps/run_tasks.py
@@ -6,11 +6,11 @@ from src.utils.tasks.task_orchestrator import TaskOrchestrator
 if __name__ == "__main__":
     tasks = [
         LiveIntradayTradesTask(),
-        DABorderFlowsJAO(executiontime='14:00:00'),
-        DANetpositionsJAO(executiontime='14:01:00'),
-        NTCJAO(executiontime='14:02:00'),
-        ATCJAO(executiontime='14:03:00'),
-        UploadUKBorderFlowsRNP(frequency=15*60)
+        #DABorderFlowsJAO(executiontime='14:00:00'),
+        #DANetpositionsJAO(executiontime='14:01:00'),
+        #NTCJAO(executiontime='14:02:00'),
+        #ATCJAO(executiontime='14:03:00'),
+        #UploadUKBorderFlowsRNP(frequency=15*60)
     ]
 
     executor = TaskOrchestrator(tasks)
